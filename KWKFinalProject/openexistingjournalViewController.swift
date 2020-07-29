52e192f35d74bf891b2cae4f521be45040338fe2
Rhea <rheadk8@icloud.com>
2020-07-30 00:01:43 +0800

    new view controller (open existing journal)

---
+++ KWKFinalProject/openexistingjournalViewController.swift
@@ -0,0 +1,30 @@
+//
+//  openexistingjournalViewController.swift
+//  KWKFinalProject
+//
+//  Created by Rhea Kundamal on 29/7/2020.
+//  Copyright © 2020 Aishwarya Arvind. All rights reserved.
+//
+
+import UIKit
+
+class openexistingjournalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
